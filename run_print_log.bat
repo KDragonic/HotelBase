67e453f2c4e7148d53fdd06194128467a85bd8e1
Денис Коропов <denkoropov@gmail.com>
2023-06-10 16:25:27 +0300

    Правка

--- run_print_log.bat
+++ run_print_log.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d C:\\Users\\WeB Voin\\Desktop\\HotelBase\\r_logs
-for %%i in (*.log) do start cmd /c py "C:\\Users\\WeB Voin\\Desktop\\HotelBase\\print_log.py" "%%i"
+cd /d "C:\\Users\\%USERNAME%\\Desktop\\HotelBase\\r_logs"
+for %%i in (*.log) do start cmd /c py "C:\\Users\\%USERNAME%\\Desktop\\HotelBase\\print_log.py" "%%i"
